b596e8450b57113e8a6008f7ad6c0f6d116e5b2d
FerrariMarc0 <marco1010@live.it>
2023-09-25 15:18:11 +0200

    calculator-class

---
+++ net-calculator-tester/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_calculator_tester
+{
+    internal class Calculator
+    {
+        public float Add(float number1, float number2)
+        {
+            return number1 + number2;
+        }
+        public float Subtract (float number1, float number2)
+        {
+            return (number1 - number2);
+        }
+        public float Divide (float number1, float number2)
+        {
+            return number1 / number2;
+        }
+        public float Multiply (float number1, float number2)
+        {
+            return number1 * number2;
+        }
+    }
+}
